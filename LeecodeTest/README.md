d5f03ded4fc456efd4177552b03175b9f7d0b740
Pengfei Wang <pengfei.wang@anker.com>
2022-01-05 17:49:20 +0800

    直接运行java修改

---
+++ LeecodeTest/README.md
@@ -0,0 +1,23 @@
+# Leecode
+
+## 简单
+
+目录：com.wpf.leecode.low
+
+- 20220105：两数之和
+
+
+
+## 中等
+
+目录：com.wpf.leecode.middle
+
+- 
+
+
+
+## 难
+
+目录：com.wpf.leecode.high
+
+- 
---
+++ LeecodeTest/leecode/src/main/java/com/wpf/leecode/low/AddSum.java
@@ -0,0 +1,25 @@
+package com.wpf.leecode.low;
+
+/**
+ * Author: feipeng.wang
+ * Time:   2022/1/5
+ * Description : leecode链接：https://leetcode-cn.com/problems/two-sum/
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ *
+ * 你可以按任意顺序返回答案。
+ */
+public class AddSum {
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        twoSum(nums,target);
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        int[] data = new int[2];
+        System.out.print("1111");
+        return data;
+    }
+}
